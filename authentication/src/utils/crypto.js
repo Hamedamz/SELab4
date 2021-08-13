0e8648a9811c05460e817e531054dc891c950e0e
Hamed AMZ <amzhamed@gmail.com>
2021-08-14 00:30:27 +0430

    feat: add jwt token generator

--- authentication/src/utils/crypto.js
+++ authentication/src/utils/crypto.js
@@ -1,4 +1,7 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const passwordSalt = process.env.JWT_SALT || 'jwt_secret';
 
 exports.comparePassword = (password, hashPassword) => {
   return bcrypt.compareSync(password, hashPassword);
@@ -7,3 +10,9 @@ exports.comparePassword = (password, hashPassword) => {
 exports.getSaltHashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
+
+exports.getJWTToken = (payload) => {
+  return jwt.sign(payload, passwordSalt, { expiresIn: '1d' });
+};
+
+exports.passwordSalt = passwordSalt;
