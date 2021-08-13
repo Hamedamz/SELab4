290aaea851dcc7a3696ce88e7cc5a180aff00b01
Hamed AMZ <amzhamed@gmail.com>
2021-08-13 23:11:56 +0430

    feat: add a util to calculate hash of password

--- authentication/src/utils/crypto.js
+++ authentication/src/utils/crypto.js
@@ -3,3 +3,7 @@ const bcrypt = require('bcrypt');
 exports.comparePassword = (password, hashPassword) => {
   return bcrypt.compareSync(password, hashPassword);
 };
+
+exports.getSaltHashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
