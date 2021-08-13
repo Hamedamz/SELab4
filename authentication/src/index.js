f18970d6dc66a696edd6b8ee5c4a7e4c957ede36
Hamed AMZ <amzhamed@gmail.com>
2021-08-13 21:01:24 +0430

    feat: init mongodb

--- authentication/src/index.js
+++ authentication/src/index.js
@@ -1,7 +1,11 @@
 const app = require('./server');
 
+const { connectToDataBase } = require('./db');
+
 const port = 3000;
 
 app.listen(port, async () => {
+    await connectToDataBase();
+
     console.log(`Auth service listening at http://localhost:${port}`);
 });
