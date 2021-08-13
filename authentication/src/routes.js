b6e43a08dc19919426aff1c54b23dcaf9377aa93
Hamed AMZ <amzhamed@gmail.com>
2021-08-14 00:32:29 +0430

    feat: add login route

--- authentication/src/routes.js
+++ authentication/src/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const {registerUser} = require('./auth');
+const { registerUser, getTokenFromUsernameAndPassword} = require('./auth');
 
 const authRoutes = express.Router();
 
-authRoutes.post('/signup', async function (req, res, next) {
+authRoutes.post('/signup', function (req, res, next) {
   const {
     username,
     password,
@@ -13,6 +13,14 @@ authRoutes.post('/signup', async function (req, res, next) {
 
   registerUser(username, password, mobile, email)
     .then((user) => res.status(201).json(user))
+    .catch(next);
+});
+
+authRoutes.post('/login', function (req, res, next) {
+  const { username, password } = req.body;
+
+  getTokenFromUsernameAndPassword(username, password)
+    .then((data) => res.status(200).json(data))
     .catch(next);
 });
 
