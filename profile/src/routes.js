a8e9c37699f8c17a21797c6e3aa866307453bbbd
arshiamoghimi <arshiamoghimi98@gmail.com>
2021-08-14 22:43:31 +0430

    feat: updated profile routes

--- profile/src/routes.js
+++ profile/src/routes.js
@@ -1,12 +1,34 @@
 const express = require('express');
-const { getProfile } = require('./profile');
+
+const {
+  getProfile,
+  updateProfile,
+  getAllUsers,
+} = require('./profile');
 
 const profileRoutes = express.Router();
 
 profileRoutes.get('/profile', function (req, res, next) {
-  const { username } = req.headers;
+  const { role } = req.headers;
 
-  getProfile(username)
+  getAllUsers(role)
+    .then((users) => res.status(200).json(users))
+    .catch(next);
+});
+
+profileRoutes.get('/profile/:userId', function (req, res, next) {
+  const { userId } = req.params;
+
+  getProfile(userId)
+    .then((user) => res.status(200).json(user))
+    .catch(next);
+});
+
+profileRoutes.put('/profile/:userId', function (req, res, next) {
+  const { userId } = req.params;
+  const newUser = req.body;
+
+  updateProfile(userId, newUser)
     .then((user) => res.status(200).json(user))
     .catch(next);
 });
