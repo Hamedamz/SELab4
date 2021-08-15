0012dbdb6d665d76cd18db8f88236a90357b0fe0
Hamed AMZ <amzhamed@gmail.com>
2021-08-15 22:00:23 +0430

    config axios

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,61 +1,27 @@
-"use strict";
-
 import Vue from 'vue';
-import axios from "axios";
+import axios from 'axios';
 
-// Full config:  https://github.com/axios/axios#request-config
-// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
-let config = {
-  // baseURL: process.env.baseURL || process.env.apiUrl || ""
-  // timeout: 60 * 1000, // Timeout
-  // withCredentials: true, // Check cross-site Access-Control
+const conf = {
+  // baseURL: 'http://localhost:8080',
 };
 
-const _axios = axios.create(config);
+const axiosInstance = axios.create(conf);
 
-_axios.interceptors.request.use(
-  function(config) {
-    // Do something before request is sent
-    return config;
-  },
-  function(error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error),
 );
 
-// Add a response interceptor
-_axios.interceptors.response.use(
-  function(response) {
-    // Do something with response data
-    return response;
-  },
-  function(error) {
-    // Do something with response error
-    return Promise.reject(error);
-  }
-);
-
-Plugin.install = function(Vue, options) {
-  Vue.axios = _axios;
-  window.axios = _axios;
-  Object.defineProperties(Vue.prototype, {
-    axios: {
-      get() {
-        return _axios;
-      }
-    },
+Plugin.install = (VueInstance) => {
+  Object.defineProperties(VueInstance.prototype, {
     $axios: {
       get() {
-        return _axios;
-      }
+        return axiosInstance;
+      },
     },
   });
 };
 
-Vue.use(Plugin)
+Vue.use(Plugin);
 
 export default Plugin;
